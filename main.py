082a55b76b59772924b1673ea2627efbba858076
maksimprogrammist <46560812+maksimprogrammist@users.noreply.github.com>
2019-01-10 18:33:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,5 @@ g = sys.platform
 k = "\\"
 print(sys.prefix)
 print(f"{(sys.prefix).split(sep=k)[2]}, теперь я знаю твоё имя............")
-if "linux" in g:
-    print("оу май...")
-
-print('Hello мир')
 
 
