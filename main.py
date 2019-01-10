b435b839f8ccab569ff3057ec946c5d595c12143
DedKo80 <aksenovms@gmail.com>
2019-01-10 18:27:01 +0700

    организовал вывод фразы "Hello world" еще раз

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import sys
 
 print('Hello world')
 
-for i in range(10):
-    print(i)
+# for i in range(10):
+#     print(i)
+
 g = sys.platform
 k = "\\"
+print(sys.prefix)
 print(f"{(sys.prefix).split(sep=k)[2]}, теперь я знаю твоё имя..................................")
 if "linux" in g:
     print("оу май.................................................................")
