0bbb54a655c645b7246ca33b538b4aa02789b646
mariklolik <marshelo44@gmail.com>
2019-01-10 18:10:09 +0700

    линуксоиды

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import sys
+
+
 print('Hello world')
 
 for i in range(10):
     print(i)
+g = sys.platform
+if "linux" in g:
+    print("оу май.................................................................")
+
+
 
 # эту строчку я добавил с сайта
 # de pranque
